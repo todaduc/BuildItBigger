0cdf6ef66422128c105305376bef648e03a7ce20
todaduc <todaduc@gmail.com>
2018-01-11 11:57:05 -0500

    add a Toast to the android library

--- jokedisplayerlib/src/main/java/com/todaduc/jokedisplayerlib/JokeDisplayActivity.java
+++ jokedisplayerlib/src/main/java/com/todaduc/jokedisplayerlib/JokeDisplayActivity.java
@@ -2,6 +2,7 @@ package com.todaduc.jokedisplayerlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class JokeDisplayActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class JokeDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_joke);
+        if (getIntent().hasExtra("joke")){
+            Toast.makeText(this, getIntent().getStringExtra("joke"), Toast.LENGTH_SHORT).show();
+        }
     }
 }
